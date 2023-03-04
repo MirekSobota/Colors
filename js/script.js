e7fc070b15c34a5cab2024783bd49482232556be
MirekSobota <miroslaw.sobota@gmail.com>
2023-03-04 12:24:11 +0100

    adding render(), init(), newColor(),

---
+++ js/script.js
@@ -0,0 +1,50 @@
+{
+    let colors = [];
+
+    const addNewColor = (newColorInput) => {
+        colors = [
+            ...colors,
+            { content: newColorInput }
+        ];
+        render();
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const color of colors){
+            htmlString += `
+            <li class="colorList">
+             ${color.content}
+            </li>
+            `;
+        };
+
+        document.querySelector(".js-color").innerHTML = htmlString;
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault()
+
+        const newColorInputElement = document.querySelector(".js-colorInput");
+        const newColorInput = newColorInputElement.value.trim();
+
+        if (newColorInput !== "") {
+            addNewColor(newColorInput);
+        }
+        newColorInputElement.focus();
+        newColorInputElement.value = "";
+    };
+
+    const init = () => {
+        render();
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+
+
+
+    };
+
+    init();
+}
